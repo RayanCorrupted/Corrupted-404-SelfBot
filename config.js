a6ff38822a60a1e782a99bb3c9a2e4fae8ebd27a
RayanCorrupted <69820318+RayanCorrupted@users.noreply.github.com>
2020-08-17 22:37:43 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/
+Vous devez Remplir les champs ci-dessous en laissant les guillemets
+
+
+const token = ""; ///Votre token ici
+const prefix = ""  ///tu met ton prefix ici 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {//NE PAS TOUCHER
+token,        //NE PAS TOUCHER
+prefix        //NE PAS TOUCHER
+}                 //NE PAS TOUCHER
