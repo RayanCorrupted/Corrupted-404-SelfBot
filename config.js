d85eec3d8f3d9080d6300276f48607e8f1a0cbcf
RayanCorrupted <69820318+RayanCorrupted@users.noreply.github.com>
2020-08-18 00:36:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 Vous devez Remplir les champs ci-dessous en laissant les guillemets
 */
 
-const token = "NTE1ODkzMjA0NjI1NjUzNzkw.XvybAA.UIGAh1AyR1V54l8fCC1hCbIenHU"; ///tu met ton token ici
-const prefix = "/"  ///tu met ton prefix ici 
+const token = ""; ///tu met ton token ici
+const prefix = ""  ///tu met ton prefix ici 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
